cf8de21519fc70911ba26cb84471cdc56893f715
Trung <trung@Hibikiyoyo.local>
2024-03-23 18:33:15 +0700

    update terraform

--- project/starter-code/zone1/modules/rds-p/rds.tf
+++ project/starter-code/zone1/modules/rds-p/rds.tf
@@ -43,7 +43,7 @@ resource "aws_rds_cluster" "udacity_cluster" {
   vpc_security_group_ids   = [aws_security_group.db_sg_1.id]
   db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
   engine_mode              = "provisioned"
-  engine_version           = "5.6.mysql_aurora.1.19.1" 
+  engine_version           = "5.7.mysql_aurora.2.12.2" 
   engine                   = "aurora-mysql"
   skip_final_snapshot      = true
   storage_encrypted        = false
--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -24,13 +24,13 @@ resource "aws_db_subnet_group" "udacity_db_subnet_group" {
 
 resource "aws_rds_cluster" "udacity_cluster-s" {
   cluster_identifier       = "udacity-db-cluster-s"
-  availability_zones       = ["us-west-1b"]
+  availability_zones       = ["us-west-1a", "us-west-1c"]
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg-s.name
   vpc_security_group_ids   = [aws_security_group.db_sg_2.id]
   db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
   engine                   = "aurora-mysql"
   engine_mode              = "provisioned"
-  engine_version           = "5.6.mysql_aurora.1.19.1" 
+  engine_version           = "5.7.mysql_aurora.2.12.2" 
   skip_final_snapshot      = true
   storage_encrypted        = false
   depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
